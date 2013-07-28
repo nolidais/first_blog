07f08e1a7cd984d75f981ba351a714a5b98e2d06
nolidais <nolidais@gmail.com>
2013-07-28 19:59:01 +0100

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Blog::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.precompile += %w( new_manifest.css )
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
